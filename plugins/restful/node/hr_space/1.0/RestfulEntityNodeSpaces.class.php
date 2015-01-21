2abc52b05b6fee04e5661f246f7746292898072c
Guillaume Viguier <guillaume@viguierjust.com>
2015-01-21 14:25:11 +0100

    Exposed global spaces in the API

---
+++ plugins/restful/node/hr_space/1.0/RestfulEntityNodeSpaces.class.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * @file
+ * Contains \RestfulEntityNodeSpaces.
+ */
+
+class RestfulEntityNodeSpaces extends \RestfulEntityBaseNode {
+
+  /**
+   * Overrides RestfulEntityBaseNode::addExtraInfoToQuery()
+   * 
+   * Adds proper query tags
+   */
+  protected function addExtraInfoToQuery($query) {
+    parent::addExtraInfoToQuery($query);
+    $filters = $this->parseRequestForListFilter();
+    if (!empty($filters)) {
+      foreach ($query->tags as $i => $tag) {
+        if ($tag == 'node_access') {
+          unset($query->tags[$i]);
+        }
+      }
+      $query->addTag('entity_field_access');
+    }
+  }
+
+  /**
+   * Overrides \RestfulEntityBase::publicFieldsInfo().
+   */
+  public function publicFieldsInfo() {
+    $public_fields = parent::publicFieldsInfo();
+
+    $public_fields['homepage'] = array(
+      'property' => 'field_website',
+      'sub_property' => 'url',
+    );
+
+    $public_fields['email'] = array(
+      'property' => 'field_email',
+    );
+
+    $public_fields['category'] = array(
+      'property' => 'field_space_category',
+      'sub_property' => 'name',
+    );
+
+    $public_fields['organizations'] = array(
+      'property' => 'field_organizations',
+      'resource' => array(
+        'hr_organization' => 'organizations',
+      ),
+    );
+
+    $public_fields['url'] = array(
+      'property' => 'url',
+    );
+
+    return $public_fields;
+  }
+
+}
